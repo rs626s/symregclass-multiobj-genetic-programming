38b3feaa85d0daf009669757fcf1b8af4a48f8c4
rs626s <61384559+rs626s@users.noreply.github.com>
2025-07-22 02:58:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.workflow import run_experiment
+from src.workflow import run_experiment         #Import the 'run_experiment' function from the 'workflow' module located in the 'src' package
 
+#This block ensures that the code below only runs when this script is executed directly, and not when it is imported as a module in another script
 if __name__ == "__main__":
-    run_experiment()
+    run_experiment()                            #Call the 'run_experiment' function to start the experiment process
